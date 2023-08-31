03a940c15c6f2d1ba1eb61f63a7b2a0ed990dae0
changhyeon <changhyeon.h@kakao.com>
2023-08-31 14:58:12 +0900

    build: 프로젝트 초기 세팅

---
+++ src/main/java/com/programmers/ticketparis/TicketparisApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.ticketparis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketparisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketparisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/ticketparis/TicketparisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.ticketparis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketparisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
