a8229cc7be1c0c5253422ff3d17bff7427bf985c
jay-so <merry9504@naver.com>
2023-09-03 23:47:53 +0900

    feat: PerformanceController 구현

---
+++ src/main/java/com/programmers/ticketparis/controller/PerformanceController.java
@@ -0,0 +1,52 @@
+package com.programmers.ticketparis.controller;
+
+import com.programmers.ticketparis.dto.request.PerformanceCreateRequest;
+import com.programmers.ticketparis.dto.request.PerformanceUpdateRequest;
+import com.programmers.ticketparis.dto.response.PerformanceResponse;
+import com.programmers.ticketparis.service.PerformanceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/performances")
+public class PerformanceController {
+    private final PerformanceService performanceService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPerformance(@RequestBody PerformanceCreateRequest createRequest) {
+        performanceService.createPerformance(createRequest);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PerformanceResponse> findPerformanceById(@PathVariable Long id) {
+        Optional<PerformanceResponse> response = performanceService.findPerformanceById(id);
+        if (response.isPresent()) {
+            return new ResponseEntity<>(response.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public List<PerformanceResponse> findAllPerformance() {
+        return performanceService.findPerformanceAll();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<PerformanceResponse> updatePerformance(@PathVariable Long id, @RequestBody PerformanceUpdateRequest updateRequest) {
+        return new ResponseEntity<>(performanceService.updatePerformance(id, updateRequest), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerformanceById(@PathVariable Long id) {
+        performanceService.deletePerformance(id);
+    }
+
+}
