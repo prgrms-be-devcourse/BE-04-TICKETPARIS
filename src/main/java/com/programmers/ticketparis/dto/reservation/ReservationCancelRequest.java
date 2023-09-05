0f0b8f0bcdbc1ea94745f415788b32274a32ea2f
changhyeon <changhyeon.h@kakao.com>
2023-09-06 00:22:32 +0900

    feat: 공연 예매, 예매 취소 dto 작성

---
+++ src/main/java/com/programmers/ticketparis/dto/reservation/ReservationCancelRequest.java
@@ -0,0 +1,40 @@
+package com.programmers.ticketparis.dto.reservation;
+
+import com.programmers.ticketparis.domain.reservation.Reservation;
+import com.programmers.ticketparis.domain.reservation.ReservationStatus;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class ReservationCancelRequest {
+
+    @NotNull(message = "고객 ID는 빈 값일 수 없습니다.")
+    @Positive(message = "고객 ID는 1이상의 양수 값 이어야합니다.")
+    private Long customerId;
+
+    @NotNull(message = "스케줄 ID는 빈 값일 수 없습니다.")
+    @Positive(message = "스케줄 ID는 1이상의 양수 값 이어야합니다.")
+    private Long scheduleId;
+
+    @Builder
+    private ReservationCancelRequest(Long customerId, Long scheduleId) {
+        this.customerId = customerId;
+        this.scheduleId = scheduleId;
+    }
+
+    public Reservation toEntity() {
+        return Reservation.builder()
+            .reservationStatus(ReservationStatus.CANCELED)
+            .customerId(customerId)
+            .scheduleId(scheduleId)
+            .build();
+    }
+}
