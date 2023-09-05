8255bc42d849ba4a8be4eb3815b750b20b24549f
kylekim2123 <kyle2123@naver.com>
2023-09-06 03:28:28 +0900

    test: 공연장 좌석 수 조회 기능 테스트 코드 추가

--- src/test/java/com/programmers/ticketparis/repository/schedule/MybatisScheduleRepositoryTest.java
+++ src/test/java/com/programmers/ticketparis/repository/schedule/MybatisScheduleRepositoryTest.java
@@ -6,8 +6,11 @@ import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.programmers.ticketparis.domain.schedule.Schedule;
 
@@ -19,6 +22,7 @@ class MybatisScheduleRepositoryTest {
 
     @Test
     @DisplayName("스케줄 데이터를 DB에 삽입 한 후 ID를 반환한다.")
+    @Transactional
     void test_scheduleId_when_save_schedule() {
         Schedule schedule = Schedule.builder()
             .startDatetime(LocalDateTime.of(2023, 9, 11, 19, 30))
@@ -29,5 +33,13 @@ class MybatisScheduleRepositoryTest {
 
         Long scheduleId = scheduleRepository.save(schedule);
         assertThat(scheduleId).isEqualTo(schedule.getScheduleId());
+    }
+
+    @ParameterizedTest
+    @DisplayName("공연 ID를 이용해, 공연이 진행되는 공연장의 좌석 수를 조회한다.")
+    @CsvSource(value = {"1,598", "2,598", "3,200", "5,559"})
+    void test_hall_seatsCount_from_performanceId(Long performanceId, Integer expectedSeatsCount) {
+        Integer actualSeatsCount = scheduleRepository.findHallSeatsCountByPerformanceId(performanceId);
+        assertThat(actualSeatsCount).isEqualTo(expectedSeatsCount);
     }
 }
