72509eebf5d93760fd96b97fe2267671cabeddc1
kylekim2123 <kyle2123@naver.com>
2023-09-07 23:17:53 +0900

    refactor: 스케줄 생성 DTO에 검증 메시지 추가

--- src/main/java/com/programmers/ticketparis/dto/schedule/request/ScheduleCreateRequest.java
+++ src/main/java/com/programmers/ticketparis/dto/schedule/request/ScheduleCreateRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.programmers.ticketparis.domain.schedule.Schedule;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,10 +15,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ScheduleCreateRequest {
 
+    @NotNull(message = "스케줄 시작 날짜와 시간이 입력되지 않음")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDatetime;
 
-    @Positive
+    @Positive(message = "회차는 자연수만 가능")
     private Integer sequence;
 
     public Schedule toEntity(Integer seatsCount, Long performanceId) {
