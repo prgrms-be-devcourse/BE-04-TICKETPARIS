1cb132691dd03cbd10a37fca929c9b49314c82ff
charlesuu <tiger0192004@gmail.com>
2023-09-08 23:16:29 +0900

    refactor: 팀 컨벤션에 맞추어 회원 조회 메서드 네이밍 변경

--- src/main/java/com/programmers/ticketparis/service/member/CustomerService.java
+++ src/main/java/com/programmers/ticketparis/service/member/CustomerService.java
@@ -20,24 +20,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CustomerService {
 
-    private final CustomerRepository customerRepository;
+	private final CustomerRepository customerRepository;
 
-    @Transactional
-    public void createAccount(CustomerCreateRequest customerCreateRequest) {
-        Customer customer = customerCreateRequest.toEntity();
-        customerRepository.createAccount(customer);
-    }
+	@Transactional
+	public void createAccount(CustomerCreateRequest customerCreateRequest) {
+		Customer customer = customerCreateRequest.toEntity();
+		customerRepository.createAccount(customer);
+	}
 
-    public CustomerResponse findById(Long customerId) {
-        return customerRepository.findById(customerId)
-            .map(CustomerResponse::new)
-            .orElseThrow(() -> new CustomerException(ExceptionRule.NOT_EXIST_CUSTOMER, List.of(String.valueOf(customerId))));
-    }
+	public CustomerResponse findCustomerById(Long customerId) {
+		return customerRepository.findById(customerId)
+			.map(CustomerResponse::new)
+			.orElseThrow(
+				() -> new CustomerException(ExceptionRule.NOT_EXIST_CUSTOMER, List.of(String.valueOf(customerId))));
+	}
 
-    //todo: 추후 [명세서 우선순위:2] 기능인 update, delete 메서드 구현되면 해당 메서드 활용
-    private void existById(Long customerId) {
-        if (!customerRepository.existById(customerId)) {
-            throw new CustomerException(ExceptionRule.NOT_EXIST_CUSTOMER, List.of(String.valueOf(customerId)));
-        }
-    }
+	//todo: 추후 [명세서 우선순위:2] 기능인 update, delete 메서드 구현되면 해당 메서드 활용
+	private void existById(Long customerId) {
+		if (!customerRepository.existById(customerId)) {
+			throw new CustomerException(ExceptionRule.NOT_EXIST_CUSTOMER, List.of(String.valueOf(customerId)));
+		}
+	}
 }
