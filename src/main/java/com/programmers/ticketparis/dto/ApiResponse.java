ecc7a3d8d3a9d7c8be86f935ec9ee565709cbf2c
changhyeon <changhyeon.h@kakao.com>
2023-09-08 19:46:00 +0900

    feat: 클라이언트 정상 응답 값 통일을 위한 공통 Response 생성

--- src/main/java/com/programmers/ticketparis/dto/ApiResponse.java
+++ src/main/java/com/programmers/ticketparis/dto/ApiResponse.java
@@ -1,15 +1,20 @@
 package com.programmers.ticketparis.dto;
 
-import java.time.LocalDateTime;
-
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
-@AllArgsConstructor(staticName = "of")
 public class ApiResponse<T> {
 
     private final LocalDateTime timestamp = LocalDateTime.now();
     private String path;
     private T data;
+
+    @Builder
+    public ApiResponse(String path, T data) {
+        this.path = path;
+        this.data = data;
+    }
 }
