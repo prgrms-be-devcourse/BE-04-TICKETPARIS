2f7ac96907230f47adaab0442f99db601c3ee788
changhyeon <changhyeon.h@kakao.com>
2023-09-09 14:25:25 +0900

    test: 예매 상태 예외 테스트 수정

--- src/test/java/com/programmers/ticketparis/domain/reservation/ReservationTest.java
+++ src/test/java/com/programmers/ticketparis/domain/reservation/ReservationTest.java
@@ -1,11 +1,11 @@
 package com.programmers.ticketparis.domain.reservation;
 
-import com.programmers.ticketparis.exception.ReservationException;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import com.programmers.ticketparis.exception.ReservationException;
 
 class ReservationTest {
 
@@ -14,10 +14,10 @@ class ReservationTest {
     void createReservation_Success() {
         // given & when
         Reservation reservation = Reservation.builder()
-                .reservationStatus(ReservationStatus.COMPLETED)
-                .scheduleId(1L)
-                .customerId(1L)
-                .build();
+            .reservationStatus(ReservationStatus.COMPLETED)
+            .scheduleId(1L)
+            .customerId(1L)
+            .build();
 
         // then
         assertThat(reservation).isInstanceOf(Reservation.class);
@@ -27,26 +27,18 @@ class ReservationTest {
     @DisplayName("예매 상태는 예매 완료와 예매 취소가 아닌 경우 예외를 발생시킨다.")
     void anotherReservationStatus_Exception() {
         // given & when & then
-        assertThatThrownBy(() -> {
-            Reservation.builder()
-                    .reservationStatus(ReservationStatus.valueOf("Proceeding"))
-                    .scheduleId(1L)
-                    .customerId(1L)
-                    .build();
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> ReservationStatus.valueOf("Proceeding"))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("예매를 생성할 때는 항상 예매 상태가 완료여야하며 예매 취소일 경우 예외가 발생한다.")
     void notCompletedReservationStatus_Exception() {
         // given & when & then
-        assertThatThrownBy(() ->
-                Reservation.builder()
-                        .reservationStatus(ReservationStatus.CANCELED)
-                        .scheduleId(1L)
-                        .customerId(1L)
-                        .build()
-        ).isInstanceOf(ReservationException.class)
-                .hasMessage("존재하지 않는 예약 상태");
+        assertThatThrownBy(() -> Reservation.builder()
+            .reservationStatus(ReservationStatus.CANCELED)
+            .scheduleId(1L)
+            .customerId(1L)
+            .build()).isInstanceOf(ReservationException.class).hasMessage("존재하지 않는 예약 상태");
     }
 }
