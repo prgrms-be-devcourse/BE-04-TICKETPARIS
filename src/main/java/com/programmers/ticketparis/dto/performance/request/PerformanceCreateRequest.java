7a7174893e7dd59208263f5883f8c6c9f1e663d4
jay-so <merry9504@naver.com>
2023-09-10 17:46:11 +0900

    refactor (PerformanceCreateRequest & PerformanceUpdateRequest ): Bean Validation 수정 및 불필요한 변수 제거 리팩토링

--- src/main/java/com/programmers/ticketparis/dto/performance/request/PerformanceCreateRequest.java
+++ src/main/java/com/programmers/ticketparis/dto/performance/request/PerformanceCreateRequest.java
@@ -2,12 +2,8 @@ package com.programmers.ticketparis.dto.performance.request;
 
 import com.programmers.ticketparis.domain.performance.Category;
 import com.programmers.ticketparis.domain.performance.Performance;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,14 +11,14 @@ import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class PerformanceCreateRequest {
 
-    @NotNull(message = "공연의 제목은 비어있으면 안됩니다!")
+    @NotBlank(message = "공연의 제목은 필수입니다.")
     @Size(min = 1, max = 100, message = "공연 제목은 1이상 100 이하의 글자를 입력해주세요!")
     private String title;
 
-    @Size(max = 500, message = "포스터 URL은 500자 이하로 입력해주세요.")
+    @NotBlank(message = "공연 포스터의 URL은 필수입니다.")
+    @Size(max = 500, message = "공연 포스터의 URL은 500자 이하로 입력해주세요.")
     private String posterUrl;
 
     @NotNull(message = "공연의 시작 날짜는 비어둘 수 없습니다!")
@@ -31,14 +27,17 @@ public class PerformanceCreateRequest {
     @NotNull(message = "공연의 종료 날짜는 비어둘 수 없습니다!")
     private LocalDate endDate;
 
+    @NotBlank(message = "공연 시간은 필수입니다.")
     @Size(max = 10, message = "공연 시간은 10글자 이하만 입력 가능합니다")
     private String duration;
 
-    @Min(value = 0, message = "연령 등급은 0보다 커야 합니다.")
-    @Max(value = 19, message = "연령 등급은 19세 이하여야 합니다.")
+    @NotNull(message = "연령 정보는 필수입니다.")
+    @PositiveOrZero(message = "연령 등급는 0이상 이어야 합니다.")
+    @Max(value = 19, message = "연령 등급 19세 이하까지 입니다.")
     private Integer ageRating;
 
-    @Min(value = 0, message = "공연 가격은 0이상 이어야 합니다.")
+    @NotNull(message = "가격 정보는 필수입니다.")
+    @PositiveOrZero(message = "가격의 정보는 0이상 이어야 합니다.")
     private Integer price;
 
     private Category category;
@@ -51,7 +50,6 @@ public class PerformanceCreateRequest {
     private Long hallId;
 
     public Performance toEntity() {
-
         return Performance.builder()
                 .title(title)
                 .posterUrl(posterUrl)
--- src/main/java/com/programmers/ticketparis/dto/performance/request/PerformanceUpdateRequest.java
+++ src/main/java/com/programmers/ticketparis/dto/performance/request/PerformanceUpdateRequest.java
@@ -1,10 +1,7 @@
 package com.programmers.ticketparis.dto.performance.request;
 
 import com.programmers.ticketparis.domain.performance.Category;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,11 +14,12 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class PerformanceUpdateRequest {
 
-    @NotNull(message = "공연의 제목은 비어있으면 안됩니다!")
+    @NotBlank(message = "공연의 제목은 필수입니다.")
     @Size(min = 1, max = 100, message = "공연 제목은 1이상 100 이하의 글자를 입력해주세요!")
     private String title;
 
-    @Size(max = 500, message = "포스터 URL은 500자 이하로 입력해주세요.")
+    @NotBlank(message = "공연 포스터의 URL은 필수입니다.")
+    @Size(max = 500, message = "공연 포스터의 URL은 500자 이하로 입력해주세요.")
     private String posterUrl;
 
     @NotNull(message = "공연의 시작 날짜는 비어둘 수 없습니다!")
@@ -30,22 +28,22 @@ public class PerformanceUpdateRequest {
     @NotNull(message = "공연의 종료 날짜는 비어둘 수 없습니다!")
     private LocalDate endDate;
 
+    @NotBlank(message = "공연 시간은 필수입니다.")
     @Size(max = 10, message = "공연 시간은 10글자 이하만 입력 가능합니다")
     private String duration;
 
-    @Min(value = 0, message = "연령 등급은 0보다 커야 합니다.")
-    @Max(value = 19, message = "연령 등급은 19세 이하여야 합니다.")
+    @NotNull(message = "연령 정보는 필수입니다.")
+    @PositiveOrZero(message = "연령 등급는 0이상 이어야 합니다.")
+    @Max(value = 19, message = "연령 등급은 19세 이하까지 합니다.")
     private Integer ageRating;
 
-    @Min(value = 0, message = "공연 가격은 0 이상 이여야 합니다.")
+    @NotNull(message = "가격 정보는 필수입니다.")
+    @PositiveOrZero(message = "가격의 정보는 0이상 이어야 합니다.")
     private Integer price;
 
     private Category category;
 
     private String description;
-
-    @NotNull(message = "판매자의 ID 정보는 필수입니다.")
-    private Long sellerId;
 
     private Long hallId;
 }
