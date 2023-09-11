74d0b3cfc857636203874ebf48ae0d1a58a1049b
charlesuu <tiger0192004@gmail.com>
2023-09-11 21:27:14 +0900

    refactor: ApiResponse timestamp -> localDateTime으로 필드명 변경

--- src/main/java/com/programmers/ticketparis/dto/ApiResponse.java
+++ src/main/java/com/programmers/ticketparis/dto/ApiResponse.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor(staticName = "of")
 public class ApiResponse<T> {
 
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final LocalDateTime localDateTime = LocalDateTime.now();
     private String path;
     private T data;
 }
