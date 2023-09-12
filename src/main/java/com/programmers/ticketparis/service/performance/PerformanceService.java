f9c3e205083430327725e9656b79c0097efdcef5
jay-so <merry9504@naver.com>
2023-09-13 01:30:58 +0900

    refactor: PeformanceService 리팩토링

--- src/main/java/com/programmers/ticketparis/service/performance/PerformanceService.java
+++ src/main/java/com/programmers/ticketparis/service/performance/PerformanceService.java
@@ -1,64 +1,66 @@
 package com.programmers.ticketparis.service.performance;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.programmers.ticketparis.domain.performance.Performance;
 import com.programmers.ticketparis.dto.performance.request.PerformanceCreateRequest;
 import com.programmers.ticketparis.dto.performance.request.PerformanceUpdateRequest;
 import com.programmers.ticketparis.dto.performance.response.PerformanceResponse;
+import com.programmers.ticketparis.exception.ExceptionRule;
+import com.programmers.ticketparis.exception.PerformanceException;
 import com.programmers.ticketparis.repository.performance.PerformanceRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PerformanceService {
+
     private final PerformanceRepository performanceRepository;
 
     @Transactional
     public void createPerformance(PerformanceCreateRequest createRequest) {
-        if (createRequest.getStartDate().isAfter(createRequest.getEndDate())) {
-            throw new IllegalArgumentException("공연 시작 날짜는 공연 종료 날짜 이후가 될 수 없습니다.");
-        }
-
         Performance performance = createRequest.toEntity();
         performanceRepository.save(performance);
     }
 
     @Transactional
-    public PerformanceResponse updatePerformance(Long id, PerformanceUpdateRequest updateRequest) {
-        if (updateRequest.getStartDate().isAfter(updateRequest.getEndDate())) {
-            throw new IllegalArgumentException("공연 시작 날짜는 공연 종료 날짜 이후가 될 수 없습니다.");
-        }
-        performanceRepository.update(id, updateRequest);
-        return findPerformanceById(id)
-                .orElseThrow(() -> new NoSuchElementException("수정할 공연 정보를 찾지 못했습니다."));
+    public PerformanceResponse updatePerformance(Long performanceId, PerformanceUpdateRequest updateRequest) {
+        validatePerformanceExists(performanceId);
+        performanceRepository.update(performanceId, updateRequest);
+
+        return findPerformanceById(performanceId);
     }
 
-    public Optional<PerformanceResponse> findPerformanceById(Long id) {
-        Performance performance = performanceRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("해당 공연 정보는 존재하지 않습니다."));
+    public PerformanceResponse findPerformanceById(Long performanceId) {
 
-        return Optional.ofNullable(PerformanceResponse.fromEntity(performance));
+        return performanceRepository.findById(performanceId)
+            .map(PerformanceResponse::fromEntity)
+            .orElseThrow(
+                () -> new PerformanceException(ExceptionRule.NOT_FOUND_PERFORMANCE,
+                    List.of(String.valueOf(performanceId))));
     }
 
     public List<PerformanceResponse> findPerformanceAll() {
-        List<Performance> performances = performanceRepository.findAll();
-        return performances.stream()
-                .map(PerformanceResponse::fromEntity)
-                .collect(Collectors.toList());
+
+        return performanceRepository.findAll().stream()
+            .map(performance -> PerformanceResponse.fromEntity(performance))
+            .toList();
     }
 
-
     @Transactional
-    public void deletePerformance(Long id) {
-        if (!performanceRepository.findById(id).isPresent()) {
-            throw new NoSuchElementException("삭제하려는 공연을 찾지 못했습니다.");
+    public void deletePerformanceById(Long performanceId) {
+        validatePerformanceExists(performanceId);
+        performanceRepository.deleteById(performanceId);
+    }
+
+    private void validatePerformanceExists(Long performanceId) {
+        if (!performanceRepository.existsById(performanceId)) {
+            throw new PerformanceException(ExceptionRule.NOT_FOUND_PERFORMANCE, List.of(String.valueOf(performanceId)));
         }
-        performanceRepository.deleteById(id);
     }
 }
