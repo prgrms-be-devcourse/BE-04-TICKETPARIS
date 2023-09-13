2374310caba3fc4d5cac596d240d2cacc82202ec
jay-so <merry9504@naver.com>
2023-09-13 21:36:59 +0900

    refactor: Performance 도메인 테스트 리팩토링

--- src/test/java/com/programmers/ticketparis/domain/performance/PerformanceTest.java
+++ src/test/java/com/programmers/ticketparis/domain/performance/PerformanceTest.java
@@ -1,80 +1,66 @@
 package com.programmers.ticketparis.domain.performance;
 
-import org.junit.jupiter.api.*;
+import static com.programmers.ticketparis.domain.performance.Category.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 
-import static com.programmers.ticketparis.domain.performance.Category.MUSICAL;
-import static com.programmers.ticketparis.domain.performance.Category.valueOf;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PerformanceTest {
 
     @Order(1)
     @Test
-    @DisplayName("공연 생성 테스트 😀")
+    @DisplayName("공연 도메인 필드에 정상적으로 입력이 되었을 때, 공연이 생성되는지 테스트")
     void createPerformanceTest() {
 
         //given & when
         Performance testPerformance = Performance.builder()
-                .title("테스트 공연 타이틀")
-                .posterUrl("https://picsum.photos/id/237/200/300\n")
-                .startDate(LocalDate.of(2023, 9, 4))
-                .endDate(LocalDate.of(2023, 12, 31))
-                .duration("2시간")
-                .ageRating(18)
-                .price(15000)
-                .category(MUSICAL)
-                .description("테스트 공연의 상세 설명입니다.")
-                .sellerId(1L)
-                .hallId(2L).build();
-
+            .title("테스트 공연 타이틀")
+            .posterUrl("https://picsum.photos/id/237/200/300\n")
+            .startDate(LocalDate.of(2023, 9, 4))
+            .endDate(LocalDate.of(2023, 12, 31))
+            .duration("2시간")
+            .ageRating(18)
+            .price(15000)
+            .category(MUSICAL)
+            .description("테스트 공연의 상세 설명입니다.")
+            .sellerId(1L)
+            .hallId(2L).build();
 
         //then
-        Performance expectedPerformance = Performance.builder()
-                .title("테스트 공연 타이틀")
-                .posterUrl("https://picsum.photos/id/237/200/300\n")
-                .startDate(LocalDate.of(2023, 9, 4))
-                .endDate(LocalDate.of(2023, 12, 31))
-                .duration("2시간")
-                .ageRating(18)
-                .price(15000)
-                .category(MUSICAL)
-                .description("테스트 공연의 상세 설명입니다.")
-                .sellerId(1L)
-                .hallId(2L).build();
+        assertThat(testPerformance).usingRecursiveComparison().isEqualTo(testPerformance);
 
-        assertThat(testPerformance)
-                .usingRecursiveComparison()
-                .isEqualTo(expectedPerformance);
     }
 
     @Order(2)
     @Test
-    @DisplayName("공연 생성 실패 테스트(지정하지 않은 Category가 들어올때) 😱")
+    @DisplayName("공연 Category필드가 Enum이 아닐때, 공연 생성이 실패되는지 테스트")
     void invalidCategory() {
         //given & when
         String invalidCategoryString = "지정되지 않은 카테고리";
 
         //then
-        assertThrows(IllegalArgumentException.class,
-                () -> {
-                    Category category = valueOf(invalidCategoryString);
-                    Performance testPerformance = Performance.builder()
-                            .title("테스트 공연 타이틀")
-                            .posterUrl("https://picsum.photos/id/237/200/300\n")
-                            .startDate(LocalDate.of(2023, 9, 4))
-                            .endDate(LocalDate.of(2023, 12, 31))
-                            .duration("2시간")
-                            .ageRating(18)
-                            .price(15000)
-                            .category(Category.valueOf(invalidCategoryString))
-                            .description("테스트 공연의 상세 설명입니다.")
-                            .sellerId(1L)
-                            .hallId(2L).build();
-                }
-        );
+        assertThatThrownBy(() -> {
+                Category category = Category.valueOf(invalidCategoryString);
+                Performance testPerformance = Performance.builder()
+                    .title("테스트 공연 타이틀")
+                    .posterUrl("https://picsum.photos/id/237/200/300")
+                    .startDate(LocalDate.of(2023, 9, 4))
+                    .endDate(LocalDate.of(2023, 12, 31))
+                    .duration("2시간")
+                    .ageRating(18)
+                    .price(15000)
+                    .category(Category.valueOf(invalidCategoryString))
+                    .description("테스트 공연의 상세 설명입니다.")
+                    .sellerId(1L)
+                    .hallId(2L).build();
+            }
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
