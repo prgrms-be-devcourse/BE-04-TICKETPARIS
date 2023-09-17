80b7b02fdcd721a56d76d297339ce481edb6bea4
jay-so <merry9504@naver.com>
2023-09-17 20:30:24 +0900

    chore: Swagger 관련 설정

--- src/main/java/com/programmers/ticketparis/config/SwaggerConfig.java
+++ src/main/java/com/programmers/ticketparis/config/SwaggerConfig.java
@@ -1,21 +1,34 @@
 package com.programmers.ticketparis.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.OAS_30)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
     }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Springdoc 테스트")
+            .description("Springdoc을 사용한 Swagger UI 테스트")
+            .version("1.0.0");
+    }
+
+//    @Bean
+//    public Docket apiDocket() {
+//        return new Docket(DocumentationType.OAS_30)
+//            .select()
+//            .apis(RequestHandlerSelectors.basePackage("com.programmers.ticketparis.controller"))
+//            .paths(PathSelectors.any())
+//            .build();
+//    }
 }
