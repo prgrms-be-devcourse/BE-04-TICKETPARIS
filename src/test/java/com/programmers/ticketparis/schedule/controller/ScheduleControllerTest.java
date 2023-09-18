7b89369899aaba0a964d3ca29c98ccc8f9e53ee3
jay-so <merry9504@naver.com>
2023-09-18 20:50:12 +0900

    Test: ScheduleController 테스트 코드 구현

--- src/test/java/com/programmers/ticketparis/schedule/controller/ScheduleControllerTest.java
+++ src/test/java/com/programmers/ticketparis/schedule/controller/ScheduleControllerTest.java
@@ -1,20 +1,34 @@
 package com.programmers.ticketparis.schedule.controller;
 
 import com.programmers.ticketparis.schedule.dto.request.ScheduleCreateRequest;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ScheduleControllerTest {
 
-    static String baseURI = "http://localhost:8080/api/schedules";
+    private static final int PAGE_NUM = 1;
+
+    @LocalServerPort
+    public int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
+    @Order(1)
     void createScheduleTest() {
         LocalDateTime startDatetime = LocalDateTime.of(2023, 12, 12, 10, 0);
         Integer sequence = 1;
@@ -22,10 +36,11 @@ public class ScheduleControllerTest {
         ScheduleCreateRequest scheduleCreateRequest = new ScheduleCreateRequest(startDatetime, sequence, performanceId);
 
         given()
+            .port(port)
             .contentType(ContentType.JSON)
             .body(scheduleCreateRequest)
             .when()
-            .post(baseURI)
+            .post("/api/schedules")
             .then()
             .statusCode(HttpStatus.CREATED.value())
             .body("localDateTime", notNullValue())
@@ -35,10 +50,11 @@ public class ScheduleControllerTest {
     }
 
     @Test
+    @Order(2)
     void findSchedulesByPageTest() {
-        int pageNum = 1;
-        int size = 10;
-        String findSchedulesByPageURI = baseURI + "?pageNum=" + pageNum + "&size=" + size;
+        int size = 8;
+
+        String findSchedulesByPageURI = "/api/schedules?pageNum=" + PAGE_NUM + "&size=" + size;
 
         given()
             .when()
@@ -51,10 +67,11 @@ public class ScheduleControllerTest {
     }
 
     @Test
+    @Order(3)
     void findReservationsByScheduleIdByPageTest() {
-        int pageNum = 1;
         int size = 2;
-        String findReservationsByScheduleIdByPageURI = baseURI + "?pageNum=" + pageNum + "&size=" + size;
+
+        String findReservationsByScheduleIdByPageURI = "/api/schedules/1/reservations?pageNum=" + PAGE_NUM + "&size=" + size;
 
         given()
             .when()
@@ -62,15 +79,16 @@ public class ScheduleControllerTest {
             .then()
             .statusCode(HttpStatus.OK.value())
             .body("localDateTime", notNullValue())
-            .body("path", equalTo("/api/schedules"))
+            .body("path", equalTo("/api/schedules/1/reservations"))
             .body("data.size()", equalTo(2));
     }
 
     @Test
+    @Order(4)
     void deleteScheduleByIdTest() {
         Long scheduleId = 7L;
 
-        String deleteScheduleByIdURI = baseURI + "/" + scheduleId;
+        String deleteScheduleByIdURI = "/api/schedules/" + scheduleId;
 
         given()
             .when()
