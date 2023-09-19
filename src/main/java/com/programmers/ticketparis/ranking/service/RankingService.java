1cf85246096a1dd33a6bdacd67c1b5cd5a376fba
kylekim2123 <kyle2123@naver.com>
2023-09-19 16:12:59 +0900

    feat: 일정 주기 마다 캐시 데이터 생성하고 해당 캐시를 반환하는 기능 생성

--- src/main/java/com/programmers/ticketparis/ranking/service/RankingService.java
+++ src/main/java/com/programmers/ticketparis/ranking/service/RankingService.java
@@ -1,48 +1,21 @@
 package com.programmers.ticketparis.ranking.service;
 
-import static com.programmers.ticketparis.ranking.service.RankingCacheKeyGenerator.*;
-
-import java.text.MessageFormat;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.programmers.ticketparis.ranking.dto.response.RankingResponse;
-import com.programmers.ticketparis.ranking.repository.RankingRepository;
+import com.programmers.ticketparis.ranking.service.cache.RedisRankingCache;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class RankingService {
 
-    private static final int INTERVAL_HOURS = 1;
-    private static final String TOP_RANKING_CACHE_ALIAS = "topRankings";
+    private final RedisRankingCache rankingCache;
 
-    private final RankingRepository rankingRepository;
-    private final RedisTemplate<String, String> redisTemplate;
-
-    @Cacheable(value = TOP_RANKING_CACHE_ALIAS, keyGenerator = "rankingCacheKeyGenerator")
     public List<RankingResponse> findTopRankingPerformances() {
-        return rankingRepository.findTopRankingPerformances();
-    }
-
-    @Scheduled(fixedDelay = INTERVAL_HOURS, timeUnit = TimeUnit.HOURS)
-    public void updateTopRankingCachePeriodically() {
-        String topRankingCacheFullKey = MessageFormat.format("{0}::{1}", TOP_RANKING_CACHE_ALIAS,
-            getTopRankingCacheKey());
-
-        redisTemplate.delete(topRankingCacheFullKey);
-        List<RankingResponse> topRankingPerformances = findTopRankingPerformances();
-
-        log.info("[REDIS] {}개의 실시간 랭킹 정보 갱신", topRankingPerformances.size());
+        return rankingCache.getRankingFromCache();
     }
 }
