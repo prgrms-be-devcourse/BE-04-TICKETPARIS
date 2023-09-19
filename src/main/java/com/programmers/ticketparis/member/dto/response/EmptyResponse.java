17a8e41e766e338b8e94c6851c0c1a6f88f3afaf
charlesuu <tiger0192004@gmail.com>
2023-09-20 00:15:57 +0900

    feat: EmptyResponse, MemberRule enum 구현

---
+++ src/main/java/com/programmers/ticketparis/member/dto/response/EmptyResponse.java
@@ -0,0 +1,11 @@
+package com.programmers.ticketparis.member.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmptyResponse {
+
+    private String empty = "empty";
+}
