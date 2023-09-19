94ba64f2440065fb550eddfaec5e53a9d4525b6a
jay-so <merry9504@naver.com>
2023-09-19 14:18:19 +0900

    chore: Swagger 관련 설정

--- src/main/java/com/programmers/ticketparis/config/SwaggerConfig.java
+++ src/main/java/com/programmers/ticketparis/config/SwaggerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
