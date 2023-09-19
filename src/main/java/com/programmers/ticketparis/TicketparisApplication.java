30f2ca76863f01e12d4817ef94d2e2814ce5b57c
kylekim2123 <kyle2123@naver.com>
2023-09-19 16:10:30 +0900

    refactor: 불필요한 캐시 key generator 삭제

--- src/main/java/com/programmers/ticketparis/TicketparisApplication.java
+++ src/main/java/com/programmers/ticketparis/TicketparisApplication.java
@@ -2,10 +2,8 @@ package com.programmers.ticketparis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
 @EnableScheduling
 @SpringBootApplication
 public class TicketparisApplication {
