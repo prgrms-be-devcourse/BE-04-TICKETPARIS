53a60e46513560c5e050ca6192d797d4050737de
kylekim2123 <kyle2123@naver.com>
2023-09-19 16:13:25 +0900

    fix: API 응답 시, 날짜 데이터가 배열로 응답되는 버그 수정

--- src/main/java/com/programmers/ticketparis/ranking/dto/response/RankingResponse.java
+++ src/main/java/com/programmers/ticketparis/ranking/dto/response/RankingResponse.java
@@ -3,6 +3,12 @@ package com.programmers.ticketparis.ranking.dto.response;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +20,17 @@ public class RankingResponse implements Serializable {
     private Long performanceId;
     private String title;
     private String posterUrl;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate startDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate endDate;
+
     private String hallName;
     private Double reservationRate;
     private Integer ranking;
