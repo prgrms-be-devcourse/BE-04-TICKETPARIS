1c0431eef33294ce29f2dc5358dd74ae656b2cc3
charlesuu <tiger0192004@gmail.com>
2023-09-21 17:40:03 +0900

    refactor: 로그인, 로그아웃 응답 DTO에 요청 성공했다는 메시지 추가

---
+++ src/main/java/com/programmers/ticketparis/auth/dto/LogoutSuccessResponse.java
@@ -0,0 +1,11 @@
+package com.programmers.ticketparis.auth.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LogoutSuccessResponse {
+
+    private String logout = "로그아웃 성공";
+}
