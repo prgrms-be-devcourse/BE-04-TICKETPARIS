c077f3d3c37dd1bd475c8a09226648b635669fde
charlesuu <tiger0192004@gmail.com>
2023-09-24 21:19:34 +0900

    feat: 인가 시스템 구현

--- src/main/java/com/programmers/ticketparis/auth/util/UrlToMemberRuleMatcher.java
+++ src/main/java/com/programmers/ticketparis/auth/util/UrlToMemberRuleMatcher.java
@@ -3,20 +3,29 @@ package com.programmers.ticketparis.auth.util;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import com.programmers.ticketparis.member.enums.MemberRule;
+import org.springframework.util.AntPathMatcher;
+
+import com.programmers.ticketparis.member.enums.MemberRole;
 
 public class UrlToMemberRuleMatcher {
 
-    private Map<String, MemberRule> requestUrlToMemberRoleMap = new ConcurrentHashMap<>();
+    private Map<String, MemberRole> requestUrlToMemberRoleMap = new ConcurrentHashMap<>();
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
 
     //URL와 MemberRule을 등록하는 기능(Config에서 사용)
-    public UrlToMemberRuleMatcher registerUrlRule(String URI, MemberRule memberRule) {
-        requestUrlToMemberRoleMap.put(URI, memberRule);
+    public UrlToMemberRuleMatcher registerUrlRule(String URI, MemberRole memberRole) {
+        requestUrlToMemberRoleMap.put(URI, memberRole);
         return this;
     }
 
     //URI와 MemberRule 이 저장소에 등록되어있는지 boolean으로 반환하는 기능(인터셉터에서 활용)
-    public Boolean isMatch(String requestURI, MemberRule loggedInMemberRule) {
-        return requestUrlToMemberRoleMap.get(requestURI).equals(loggedInMemberRule);
+    public Boolean isMatch(String requestURI, MemberRole loggedInMemberRole) {
+        for (Map.Entry<String, MemberRole> entry : requestUrlToMemberRoleMap.entrySet()) {
+            if (pathMatcher.match(entry.getKey(), requestURI) && entry.getValue().equals(loggedInMemberRole)) {
+                System.out.println("isMath함수 : " + loggedInMemberRole.name());
+                return true;
+            }
+        }
+        return false;
     }
 }
